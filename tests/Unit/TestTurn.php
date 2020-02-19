cbd6061cf096b8e22c7b02f034e8878ef0cbd254
Pablo López Díaz <pld7@alu.ua.es>
2020-02-19 09:10:21 +0100

    Added model Turn and a the test TestTurn

---
+++ tests/Unit/TestTurn.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTurn extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function C1_TestTurn()
+    {
+        $this->assertTrue(true);
+    }
+}

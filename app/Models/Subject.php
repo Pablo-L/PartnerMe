a02c64f0045aef163bb4a911aef9d60aacd9a4cf
PabloGil <pagg5@alu.ua.es>
2020-02-20 00:17:06 +0100

    Añado atributos a la clase Subject

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+    private $name;//nombre de la asignatura
+    private $quarter;//cuatrimestre en el que se imparte
+    private $course;//curso en el que se imparte
+    
 }

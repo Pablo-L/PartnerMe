7008d8fe1f4d2443a6657155d913cff932a3b6be
PabloGil <pagg5@alu.ua.es>
2020-02-24 13:30:18 +0100

    Añado relacion hacia turn

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -9,4 +9,7 @@ class Subject extends Model
     private $subjectName;//nombre de la asignatura
     private $department;//departamento de la asignatura
     
+    public function turns(){
+        return $this->hasMany('App\Turn');
+    }
 }

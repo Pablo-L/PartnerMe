8779122ab6e64a44b3db32e8bd6c7ef43c024dfe
PabloGil <pagg5@alu.ua.es>
2020-03-22 22:38:58 +0100

    Añado metodos a la clase Subject

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -9,6 +9,32 @@ class Subject extends Model
     private $subjectName;//nombre de la asignatura
     private $department;//departamento de la asignatura
     
+    public function __construct(){
+        //echo('Construyo asignatura'.PHP_EOL);
+        $this->subjectName = "defaultName";
+        $this->department ="defaultDepartment";
+    }
+
+    public function __construct2($sub, $dep){
+        //echo('Construyo asig'.$sub.PHP_EOL);
+        $this->subjectName = $sub;
+        $this->department = $dep;
+    }
+
+    public function __destruct(){
+        //print "Destruyendo " . $this->subjectName.PHP_EOL;
+    }
+
+    public function getSubjects(){
+        $subs = subject::get(['subjectName']);
+        return $subs;
+    }
+
+    public function getDepartment($sub){
+        $deps = subject::where('subjectName',$sub)->get('department');
+        return $deps;
+    }
+
     public function turns(){
         return $this->hasMany('App\Turn');
     }

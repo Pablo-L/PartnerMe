9911983a634a39cbb4c7c23dcc0f655577a2390e
Armen <an49@alu.ua.es>
2020-04-02 20:16:35 +0200

    Interfaz de group detail

--- resources/views/group/group-detail.blade.php
+++ resources/views/group/group-detail.blade.php
@@ -9,11 +9,11 @@
 
 @section('content')
 
-    <div class="group-container">
+    <!-- <div class="group-container">
         <div class="group-name">{{ $group->groupName }}</div>
         <div>
             <div class="group-image" style="background-image:url('/storage/group_img/{{ $group->image }}')"></div>
-            <div class="group-description">{{ $group->description }}</div>
+            <div class="group-description ">{{ $group->description }}</div>
         </div>
         <div class="group-integrantes">Integrantes</div>
         <div>
@@ -32,6 +32,42 @@
             <label>Asignatura:</label>
             <span> {{ $subject->subjectName }} </span>
         </div>
+    </div>
+    -->
+
+    <div class="group-container">
+        <div class="group-name">
+            {{ $group->groupName }}
+        </div>
+
+        <div class="group-image"></div>
+        <div class="group-description">{{ $group->description }}</div>
+
+        <div class="students-title">Integrantes</div>
+
+        <div class="group-integrantes">
+            @foreach($students as $student)
+                    <div class="group-student">
+                        <div class="group-student-photo"></div>
+                        <div class="group-student-info">{{ $student->name }}<br>Valoración: {{ $student->avgRating }}</div>
+                    </div>
+            @endforeach
+
+        </div>
+
+
+        <div class="turno_asig">
+            <div class="group-turn">
+                <label>Turno:</label>
+                <span>{{$turn->day}} {{$turn->beginHour}}-{{$turn->endHour}}</span>
+            </div>
+            <div class="group-subject">
+                <label>Asignatura:</label>
+                <span> {{ $subject->subjectName }} </span>
+            </div>
+        </div>
+
+
     </div>
 
 @endsection

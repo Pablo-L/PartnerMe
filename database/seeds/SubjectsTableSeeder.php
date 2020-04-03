1b3417c0002a007d4d21913a6dae5893566b8a10
Armen <an49@alu.ua.es>
2020-04-03 22:46:13 +0200

    Añadidas asignaturas en el seeder de subject

--- database/seeds/SubjectsTableSeeder.php
+++ database/seeds/SubjectsTableSeeder.php
@@ -14,7 +14,7 @@ class SubjectsTableSeeder extends Seeder
         //borramos datos de la tabla
         DB::table('subjects')->delete();
         //rellenamos la tabla
-        DB::table('subjects')->insert([[
+        /*DB::table('subjects')->insert([[
             'subjectName' => 'Programación 1',
             'department' => 'Ciencia de la computación e inteligencia artificial'
         ],[
@@ -53,6 +53,88 @@ class SubjectsTableSeeder extends Seeder
         ],[
             'subjectName' => 'Sistemas operativos',
             'department' => 'Tecnología informática y computación'
+        ]]);*/
+        DB::table('subjects')->insert([[
+            'subjectName' => 'Matematicas 1',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Fundamentos Fisicos De La Informatica',
+            'department'  => 'Fisica ,Ingenieria De Sistemas Y Teoria De La Señal'
+        ],[
+            'subjectName' => 'Fundamentos De Los Computadores',
+            'department'  => 'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Programacion 1',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Sistemas Y Tecnologías De Información',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Matematica Discreta',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Matematicas 2',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Programacion 2',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Fundamentos De Las Bases De Datos',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Estructura De Los Computadores',
+            'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Programacion 3',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Sistemas Operativos',
+            'department'  => 'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Diseño De Bases De Datos',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Redes De Computadores',
+            'department'  => 'Fisica ,Ingenieria De Sistemas Y Teoria De La Señal'
+        ],[
+            'subjectName' => 'Programacion Y Estructuras De Datos',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Lenguajes Y Paradigmas De Programacion',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Analisis Y Diseño De Algoritmos',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Herramientas Avanzadas Para El Desarrollo De Aplicaciones',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Arquitectura De Los Computadores',
+            'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Sistemas Distribuidos',
+            'department'  => 'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Analisis Y Especificacion De Sistemas Software',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Sistemas Inteligentes',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Administracion De Sistemas Operativos Y De Redes De Computadores',
+            'department'  => 'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Ingenieria De Los Computadores',
+            'department'  => 'Tecnología Informática Y Computación'
+        ],[
+            'subjectName' => 'Diseño De Sistemas Software',
+            'department'  => 'Lenguajes Y Sistemas Informáticos'
+        ],[
+            'subjectName' => 'Planificación Y Pruebas De Sistemas Software',
+            'department'  => 'Ciencia De La Computacion e Inteligencia Artificial'
+        ],[
+            'subjectName' => 'Gestion De Proyectos Informaticos',
+            'Ciencia De La Computacion e Inteligencia Artificial'
         ]]);
     }
 }

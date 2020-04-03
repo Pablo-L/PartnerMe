dd7f57ffadfe5540ceeed479e4fecdf6b00f609e
Armen <an49@alu.ua.es>
2020-04-03 20:53:05 +0200

    Mejorado seeder de groups_students

--- database/seeds/Groups_StudentsTableSeeder.php
+++ database/seeds/Groups_StudentsTableSeeder.php
@@ -16,14 +16,15 @@ class Groups_StudentsTableSeeder extends Seeder
         //rellenamos la tabla
         //$student;
         $groups=DB::table('groups')->get();
-        $id=1;
+        $students = DB::table('students')->get();
+        $i = 0;
         foreach($groups as $group){
-            $student=DB::table('students')->where('id',$id)->first();
+            $student=DB::table('students')->where('id',$students[$i]->id)->first();
             DB::table('group_student')->insert([
                 'group_id' => $group->id,
                 'student_id' => $student->id
             ]);
-            $id++;
+            $i++;
         }
     }
 }

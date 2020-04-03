2119a326ea928c42e1b6fe88a2bb380b7624a102
Enrique Antonio Cores Rodríguez <ecoresjr@gmail.com>
2020-04-03 16:19:27 +0200

    Estilo a vistas de create y edit group y previsualización de imagenes.

--- resources/views/group/group-detail.blade.php
+++ resources/views/group/group-detail.blade.php
@@ -9,38 +9,12 @@
 
 @section('content')
 
-    <!-- <div class="group-container">
-        <div class="group-name">{{ $group->groupName }}</div>
-        <div>
-            <div class="group-image" style="background-image:url('/storage/group_img/{{ $group->image }}')"></div>
-            <div class="group-description ">{{ $group->description }}</div>
-        </div>
-        <div class="group-integrantes">Integrantes</div>
-        <div>
-            @foreach($students as $student)
-                <div class="group-student">
-                    <div class="group-student-photo">foto</div>
-                    <div class="group-student-info">{{ $student->name }}<br>Valoración: {{ $student->avgRating }}</div>
-                </div>
-            @endforeach
-        </div>
-        <div class="group-turn">
-            <label>Turno:</label>
-            <span>{{$turn->day}} {{$turn->beginHour}}-{{$turn->endHour}}</span>
-        </div>
-        <div class="group-subject">
-            <label>Asignatura:</label>
-            <span> {{ $subject->subjectName }} </span>
-        </div>
-    </div>
-    -->
-
     <div class="group-container">
         <div class="group-name">
             {{ $group->groupName }}
         </div>
 
-        <div class="group-image"></div>
+        <div class="group-image" style="background-image:url('/storage/group_img/{{ $group->image }}')"></div>
         <div class="group-description">{{ $group->description }}</div>
 
         <div class="students-title">Integrantes</div>

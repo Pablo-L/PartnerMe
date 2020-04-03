ea8ab53df1fbcaf327c461256604c9bca428fea2
Armen <an49@alu.ua.es>
2020-04-03 23:30:11 +0200

    Corregido bug

--- resources/views/turn/turns-list-data.blade.php
+++ resources/views/turn/turns-list-data.blade.php
@@ -1,15 +1,9 @@
 @foreach($turns as $turn)
     <tr id = "{{ $turn->id}}">
         <td >{{ @$turn->classroomName}}</td>
-<<<<<<< Updated upstream
-        <td ><a href=" {{ action('TurnController@detail', ['id' => $turn->id]) }} ">{{ @$turn->day }}</a></td>
-        <td >{{ @$turn->beginHour }}</td>
-        <td >{{ @$turn->endHour }}</td>
-=======
         <td >{{ @$turn->day }}</td>
         <td >{{ DATE_FORMAT(date_create_from_format('H:i:s', $turn->beginHour), 'H:i') }}</td> 
         <td >{{ DATE_FORMAT(date_create_from_format('H:i:s', $turn->endHour), 'H:i') }}</td> 
->>>>>>> Stashed changes
         <td >{{ @$turn->subjectName }}</td>
         <td>
             <a href=" {{ action('TurnController@edit', ['id' => $turn->id]) }} "> 

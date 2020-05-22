a05171f956245202944143c3ca95731f69fce6a0
Enrique Antonio Cores Rodríguez <ecoresjr@gmail.com>
2020-05-22 19:36:21 +0200

    Página de indice

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,18 +8,13 @@
 @endsection
 
 @section('content')
-    <div class="titulo">
-        <h1>Inicio para invitados</h1>
-        <!--
-            <button class="btnLogin">
-                <a href="{{ URL::route('groupCreate') }}">Crear grupo</a>
-            </button>
-            <button class="btnLogin">
-                <a href="{{ URL::route('subjectCreate') }}">Crear asignatura</a>
-            </button>
-            <button class="btnLogin">
-                <a href="{{ URL::route('turnCreate') }}">Crear turno</a>
-            </button>
-        -->
+    <div class="indexContainer">
+        <div class="indexText">
+            <h1>Crea y colabora en grupos de tu universidad de forma sencilla</h1>
+            <p>PartnerMe ofrece un conjunto de herramientas online para que los miembros
+                 de la comunidad  universitaria puedan crear grupos de trabajo, colaborar
+                y comunicarse de forma sencilla y rápida. <a href="">Saber más...</a></p>
+        </div>
+        <img src="{{asset('imgs/indeximage.png')}}" alt="Imagen del índice" class="indexImg">
     </div>
 @endsection
